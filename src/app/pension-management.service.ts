fe32069ffaac60e886f7c2004e53ddd60fa26bf1
yogesh <yosuza143@gmail.com>
2021-08-05 14:16:31 +0530

    added all the services

--- src/app/pension-management.service.ts
+++ src/app/pension-management.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensionManagementService {
+  constructor(private http: HttpClient) { }
+  getData(data: any) {
+    console.log(data);
+    let url = "http://localhost:8085/processPensionerInput";
 
-  constructor() { }
+    let dataF = {
+      aadharNumber: data.aadharNumber,
+      dateOfBirth: data.dateOfBirth,
+      pan: data.pan,
+      pensionType: data.pensionType,
+      name: data.username
+    };
+    return this.http.post(url, dataF);
+  }
+
+  getConformation() {
+    let url: string = "http://localhost:8085/disbursement";
+    return this.http.get(url);
+  }
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  userData: any;
+  constructor() {
+    this.userData = {};
+  }
+  setUserData(val: object) {
+    this.userData = val;
+  }
+  getUserData() {
+    return this.userData;
+  }
 }
