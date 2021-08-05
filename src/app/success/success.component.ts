21eda9b950ac723a4402b1f431d3c15e7bee1f93
yogesh <yosuza143@gmail.com>
2021-08-05 14:23:59 +0530

    success updated

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticateService } from '../authenticate.service';
+import { PensionManagementService } from '../pension-management.service';
 
 @Component({
   selector: 'app-success',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private pensionService: PensionManagementService, private router: Router, private authService: AuthenticateService) { }
+  token: string = '';
   ngOnInit(): void {
+    let tok = sessionStorage.getItem('token');
+    if (tok != null)
+      this.token = tok;
+    console.log(tok);
+    console.log("tokk");
+    this.authService.validate(this.token).subscribe((response: any) => {
+      console.log(response);
+      let message = response.message;
+      console.log(message);
+      if (message != 'Success') {
+        this.router.navigate(['']);
+      }
+    }, (error: any) => {
+      this.router.navigate(['']);
+    });
   }
 
 }
+
